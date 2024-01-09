4090d08a01246154ef19a372b1a7b5321f45fb86
Alton Phan <altonphan@outlook.com>
2024-01-09 16:52:47 -0600

    create todo and get all todo routes made

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "kth18822",
+    password: "2020230",
     host: "localhost",
     port: 5432,
     database: "perntodo"
