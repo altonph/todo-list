cffde0e5e78bb160c2e4c28a7b52a6125aeb0b64
Alton Phan <altonphan@outlook.com>
2024-01-09 16:04:00 -0600

    post request recieved

--- server/index.js
+++ server/index.js
@@ -5,11 +5,19 @@ const pool = require("./db");
 
 //middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //req.body
 
 //ROUTES//
 
 //create a todo
+
+app.post("/todos", async(req, res) => {
+    try {
+        console.log(req.body);
+    } catch(err) {
+        console.error(err.message);
+    }
+});
 
 //get all todos 
 
