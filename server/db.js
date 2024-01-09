0a9e5b34ebc9a4870524120370c660a9dba8e9c5
Alton Phan <altonphan@outlook.com>
2024-01-09 17:12:10 -0600

    all api routes complete

--- server/db.js
+++ server/db.js
@@ -1,11 +1,14 @@
+//import necessry libaries
 const Pool = require("pg").Pool;
 
+//fill with data
 const pool = new Pool({
     user: "postgres",
-    password: "2020230",
+    password: "2020230", //master password 
     host: "localhost",
     port: 5432,
     database: "perntodo"
 });
 
+//export to use in other files
 module.exports = pool;
