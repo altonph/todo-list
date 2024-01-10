faee73ce48dd584cf187d8243a992d02838ce7a6
Alton Phan <altonphan@outlook.com>
2024-01-09 22:47:52 -0600

    set up client and input todo component

---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,20 @@
+import React /*,{ useState }*/ from "react";
+
+const InputTodo = () => {
+
+    //const [description, setDescription] = useState("")
+
+    return (
+        <>
+            <h1 className="text-center mt-5">Pern Todo List</h1>
+
+            <form className="d-flex mt-5">
+                <input type="text" className="form-control" />
+                <button className="btn btn-success">Add</button>
+            </form>
+
+        </>
+    );
+};
+
+export default InputTodo;
